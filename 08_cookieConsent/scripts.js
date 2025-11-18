483fed70128804d5cfe2da456702a267bbfac5ae
Ali-AlWedyan <ali.wedyan.ali@gmail.com>
2025-11-19 00:34:31 +0300

    cookie Concent

--- 08_cookieConsent/scripts.js
+++ 08_cookieConsent/scripts.js
@@ -1,6 +1,24 @@
-const cookieConsent = document.querrySelect(".cookie-consent")
-const cookieButton = document.querrySelect(".cookie-consent button");
+const cookieConsent = document.querySelector(".cookie-container");
+const cookieConsentButton = document.querySelector(".cookie-container button");
+const reset = document.querySelector(".reset");
 
-function isCookieAva(){
-    
+function isCookie(){
+    if(localStorage.getItem("cookieConsent") == "true"){
+        cookieConsent.style.display = "none";
+        return true;
+    }
+    cookieConsent.style.display = "flex";
 }
+
+cookieConsentButton.addEventListener("click", e =>{
+    localStorage.setItem("cookieConsent", "true");
+    cookieConsent.style.display = "none";
+});
+
+reset.addEventListener("click", e=> {
+    localStorage.removeItem("cookieConsent");
+    cookieConsent.style.display = "flex";
+});
+
+
+isCookie();
