17b19dbd56eacdfc602c9df75cc8cb7e48ccbfd8
Ali-AlWedyan <ali.wedyan.ali@gmail.com>
2025-11-22 17:50:57 +0300

    base concept of the accordion

---
+++ 10_Accordion/scripts.js
@@ -0,0 +1,11 @@
+const questions = document.querySelectorAll(".accordion-question");
+const arrows = document.querySelectorAll(".accordion-question > span");
+const answers = document.querySelectorAll(".accordion-answer");
+
+questions.forEach((q, i) => q.addEventListener("click", e =>{
+    arrows[i].style.rotate = arrows[i].style.rotate === "0deg" ? "90deg" : "0deg";
+    setTimeout(() =>{
+        answers[i].classList.toggle("active");
+    }, 500);
+
+}))
